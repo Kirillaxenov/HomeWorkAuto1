93ba8c2f350c96a3fff10f36b09ef685dadfd42a
Kirill <kaxyonov@gmail.com>
2023-06-13 15:14:56 +0300

    juni4

---
+++ src/test/java/CashBackHackerTests.java
@@ -0,0 +1,33 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.netology.service.CashbackHackService;
+
+public class CashBackHackerTests {
+    @Test
+    public void shouldBuyHowMuchMore() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int actual = service.remain(amount);
+        int expected = 200;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldBuyHowMuchMoreIf0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldBuyHowMuchMoreIf1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        Assert.assertEquals(expected, actual);
+    }
+}
